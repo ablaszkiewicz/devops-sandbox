11a35df6e7a1574dd2a0cdc572ee22972e4f19d8
Aleksander Blaszkiewicz <kqmdjc8@gmail.com>
2024-09-21 16:51:07 +0200

    add frontend building

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,24 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import Login from "./Login.tsx";
-import Disease from "./Disease.tsx";
-import Supported from "./Supported.tsx";
-import Chat from "./Chat.tsx";
-import Dashboard from "./Dashboard.tsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <ChakraProvider
-            theme={extendTheme({
-                config: {
-                    initialColorMode: "dark",
-                },
-            })}
-        >
-            <Dashboard />
-        </ChakraProvider>
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ChakraProvider
+      theme={extendTheme({
+        config: {
+          initialColorMode: 'dark',
+        },
+      })}
+    >
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
 );
